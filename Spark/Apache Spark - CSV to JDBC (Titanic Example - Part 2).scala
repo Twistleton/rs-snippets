6d471eed6334fcdb16d365369b9d24913e5aaaff
Twistleton <rolf.speer@rolf-benz.com>
2025-06-27 10:55:45 +0200

    Update Apache Spark - CSV to JDBC (Titanic Example - Part 2).scala

--- Spark/Apache Spark - CSV to JDBC (Titanic Example - Part 2).scala
+++ Spark/Apache Spark - CSV to JDBC (Titanic Example - Part 2).scala
@@ -1,3 +1,4 @@
+
 spark-shell --driver-class-path /root/Documents/Spark/mysql-connector-java-8.0.26.jar --jars /root/Documents/Spark/mysql-connector-java-8.0.26.jar
 
 val titanic = spark.read.format("csv").option("header", "true").option("inferSchema", "true").option("nullValue", "?").load("/root/Documents/Spark/titanic.csv")
